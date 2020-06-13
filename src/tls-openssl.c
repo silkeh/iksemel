e7981a55cdd6f5ed998eb7a3e648035f14a83f22
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-06-13 15:49:24 +0300

    Add more compatible iksemel.
    Due to @sulincix's reason I added openssl 1.0.x support. It was 
    deprecated on 97c810f6907803f67623a21f251550b81b3fda89

--- src/tls-openssl.c
+++ src/tls-openssl.c
@@ -24,9 +24,16 @@ static int init_done;
 static int
 my_bio_create (BIO *b)
 {
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	b->init = 1;
+	b->num = 0;
+	b->ptr = NULL;
+	b->flags = 0 ;
+#else
 	BIO_set_init(b, 1); //b->init = 1;
 	BIO_set_data(b, NULL); //	b->ptr = NULL;
 	BIO_set_flags(b, 0); //b->flags = 0 ;
+#endif
 	return 1;
 }
 
@@ -34,17 +41,27 @@ static int
 my_bio_destroy (BIO *b)
 {
 	if (b == 0) return 0;
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	b->ptr = NULL;
+	b->init = 0;
+	b->flags = 0;
+#else
 	BIO_set_init(b, 0); //b->init = 0;
 	BIO_set_data(b, NULL); //	b->ptr = NULL;
 	BIO_set_flags(b, 0); //b->flags = 0 ;
-
+#endif
 	return 1;
 }
 
 static int
 my_bio_read (BIO *b, char *buf, int len)
 {
-	struct ikstls_data *data = (struct ikstls_data *) BIO_get_data(b);
+	#if OPENSSL_VERSION_NUMBER < 0x10100000L
+		struct ikstls_data *data = (struct ikstls_data *) b->ptr;
+	#else
+		struct ikstls_data *data = (struct ikstls_data *) BIO_get_data (b); // b->ptr;
+	#endif
 	int ret;
 
 	if (buf == NULL || len <= 0 || data == NULL) return 0;
@@ -57,7 +74,11 @@ my_bio_read (BIO *b, char *buf, int len)
 static int
 my_bio_write (BIO *b, const char *buf, int len)
 {
-	struct ikstls_data *data = (struct ikstls_data *) BIO_get_data(b);
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	struct ikstls_data *data = (struct ikstls_data *) b->ptr;
+#else
+	struct ikstls_data *data = (struct ikstls_data *) BIO_get_data (b); // b->ptr;
+#endif
 	int ret;
 
 	if (buf == NULL || len <= 0 || data == NULL) return 0;
@@ -95,27 +116,21 @@ my_bio_puts (BIO *b, const char *str)
 	return my_bio_write (b, str, strlen(str));
 }
 
-static BIO_METHOD *my_bio_method;
-
-/* Return the method table for the iksemel transport BIO */
-static BIO_METHOD *
-BIO_s_iksemel_tbio(void)
-{
-	if (my_bio_method == NULL) {
-		my_bio_method = BIO_meth_new(( 100 | 0x400 ),
-																			 "iksemel transport");
-		if (my_bio_method == NULL)
-			return NULL;
-		BIO_meth_set_write(my_bio_method, my_bio_write);
-		BIO_meth_set_read(my_bio_method, my_bio_read);
-		BIO_meth_set_puts(my_bio_method, my_bio_puts);
-		BIO_meth_set_gets(my_bio_method, my_bio_gets);
-		BIO_meth_set_ctrl(my_bio_method, my_bio_ctrl);
-		BIO_meth_set_create(my_bio_method, my_bio_create);
-		BIO_meth_set_destroy(my_bio_method, my_bio_destroy);
-	}
-	return my_bio_method;
-}
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+static BIO_METHOD my_bio_method = {
+	( 100 | 0x400 ),
+	"iksemel transport",
+	my_bio_write,
+	my_bio_read,
+	my_bio_puts,
+	my_bio_gets,
+	my_bio_ctrl,
+	my_bio_create,
+	my_bio_destroy
+};
+#else
+static BIO_METHOD *my_bio_method; // New type bio method
+#endif
 
 static int
 tls_handshake (struct ikstls_data **datap, ikstransport *trans, void *sock)
@@ -153,8 +168,22 @@ tls_handshake (struct ikstls_data **datap, ikstransport *trans, void *sock)
 		return IKS_NOMEM;
 	}
 
-	bio = BIO_new (BIO_s_iksemel_tbio());
-	BIO_set_data(bio, (void *) data);
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	bio = BIO_new (&my_bio_method);
+	bio->ptr = (void *) data;
+#else
+	my_bio_method = BIO_meth_new (( 100 | 0x400 ), "iksemel transport");
+	BIO_meth_set_write (my_bio_method, my_bio_write);
+	BIO_meth_set_read (my_bio_method, my_bio_read);
+	BIO_meth_set_puts (my_bio_method, my_bio_puts);
+	BIO_meth_set_gets (my_bio_method, my_bio_gets);
+	BIO_meth_set_ctrl (my_bio_method, my_bio_ctrl);
+	BIO_meth_set_create (my_bio_method, my_bio_create);
+	BIO_meth_set_destroy (my_bio_method, my_bio_destroy);
+	bio = BIO_new (my_bio_method);
+	BIO_meth_free (my_bio_method);
+	BIO_set_data (bio, (void *) data);
+#endif
 	SSL_set_bio (data->ssl, bio, bio);
 	if (SSL_connect (data->ssl) < 0) {
 		SSL_free (data->ssl);
